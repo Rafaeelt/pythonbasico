44fe16c506e4054e5b39196ebf77b1eda7c1665a
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2026-02-09 14:11:16 +0000

    Aula01

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite o primeiro número "))
+num1 = int(input("Digite o segundo número "))
+
+soma = num1+num2
+
+print("A soma é : ",soma)
+
+num1 = int(input("Digite o primeiro número "))
+num1 = int(input("Digite o segundo número "))
+
+subtrair = 
---
+++ ola.py
@@ -0,0 +1,10 @@
+#nome = input("Digite seu nome: ")
+#print("Olá, ", nome ,"Seja bem vindo")
+
+nome = input("Digite seu nome: ")
+("Olá, ", nome ,"Seja bem vindo")
+print ("Olá, seu nome é: Maria Rita, seja bem vinda ")
+
+nome = input("Digite seu nome: ")
+("Olá, ", nome ,"Seja bem vindo")
+print ("Olá, qual sua idade: tenho 14 anos")
