9f0c23651ee8c5257a007aaacd3812552eabeff7
Rafael <rafael.oliveira.silva09@escola.pr.gov.br>
2026-02-24 12:08:51 +0000

    versao02

--- Advinhacao.py
+++ Advinhacao.py
@@ -3,14 +3,29 @@ print('***Jogo advinhação***********')
 print('*****************************')
 
 numero_secreto = 39
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while (rodada <= total_tentativas):
+     
+    chute_str = input("Digite o seu numero: ")
+    print("Seu numero é ", chute_str)
 
-print("Seu numero é ", chute_str)
+    chute = int(chute_str)
+     
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-churte = int(chute_str)
+    if(acertou):
+        print("Você Acertou!! ")
+        break
+    else:
+        if(maior):
+          print("O seu chute foi maior que o número_secreto")
+        elif(menor):
+          print("O seu chute foi menor que o número_secreto")
+rodada = rodada +1
 
-if(numero_secreto == chute_str):
-    print("Você Acertou!! ")
-else:
-    print("Você errou    :(")
+print("Fim de jogo!")10
+
